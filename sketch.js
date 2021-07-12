5be774f2ee7bd12ecbbe460fcbbc454595553bd5
xmeatballx <erik.jensen5@pcc.edu>
2021-07-11 17:18:08 -0700

    first commit

---
+++ sketch.js
@@ -0,0 +1,24 @@
+let i = 0;
+function setup(){
+  createCanvas(windowWidth,windowHeight);
+  r = random(PI);
+}
+
+// function mouseClicked() {
+//   save("drawing.png");
+// }
+
+function draw() {
+ //background(255);
+ i+=.01;
+ push();
+ translate(mouseX+(width/4*sin(i)),mouseY+(height/4*cos(i*r)));
+ rotate(sin(i)*HALF_PI);
+ let ly = 5;
+ ly+=sin(i*PI)*200;
+ stroke(abs(sin(i))*255, abs(sin(i))*255, abs(cos(i))*255);
+ strokeWeight(100);
+ line(0,-ly, 0, ly); 
+ pop();
+ text("control brushstroke with mouse position", width/2, 30)
+}
